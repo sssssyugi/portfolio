6eead6a1df63fd62f8c7d80df93b6ee2e284e8b6
sssssyugi <hsing06@naver.com>
2017-07-17 18:44:51 +0900

    aa

--- html/js/slide_banner.js
+++ html/js/slide_banner.js
@@ -25,7 +25,7 @@ $(function(){ // $(document).ready(function(){ });
             }
 
             button.eq(n).trigger('click');
-        }, 3000);
+        }, 2000);
     }
 
     function move(i){
